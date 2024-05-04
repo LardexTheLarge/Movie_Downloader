57a1f5a9045438ea212fff4ab8dc7b7f15c01efd
LardexTheLarge <0gabevee0@gmail.com>
2024-05-04 15:57:16 -0600

    Testing url loader

--- Downloader.py
+++ Downloader.py
@@ -1,21 +1,37 @@
 import requests
 import os
+from bs4 import BeautifulSoup
 
-file_urls = []
+gUrl = "https://archive.org/download/RecurringDinosaurInfestationFilms"
 
-save_dir = 'c:\Users\Lardex\Videos\Captures'
+response = requests.get(gUrl)
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-def download_file(url):
-    local_filename = url.split('/')[-1]
-    local_filepath = os.path.join(save_dir, local_filename)
-    with requests.get(url, stream = True) as r:
-        r.raise_for_status()
-        with open(local_filepath, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
+video_tags = soup.find_all('a')
+
+for video_tag in video_tags:
+    source_tags = video_tag.find_all('a')
+    for source_tag in source_tags:
+        video_url = source_tag.get('a')
+        if video_url.endswith('.mp4'):
+            print(f"MP4 URL: {video_url}")
+
+# file_urls = []
+
+# save_dir = 'c:\Users\Lardex\Videos\Captures'
+
+# if not os.path.exists(save_dir):
+#     os.makedirs(save_dir)
+
+# def download_file(url):
+#     local_filename = url.split('/')[-1]
+#     local_filepath = os.path.join(save_dir, local_filename)
+#     with requests.get(url, stream = True) as r:
+#         r.raise_for_status()
+#         with open(local_filepath, 'wb') as f:
+#             for chunk in r.iter_content(chunk_size=8192):
+#                 f.write(chunk)
             
-    return local_filepath
+#     return local_filepath
     
